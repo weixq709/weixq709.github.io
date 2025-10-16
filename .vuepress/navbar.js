cc65445ae62925e03700a82e17958b39c81fc9f7
weixiaoqiang <weixiaoqiang8311@163.com>
2025-10-16 22:22:58 +0800

    refactor: 调整笔记路径、修改文章错误

--- .vuepress/navbar.js
+++ .vuepress/navbar.js
@@ -10,7 +10,7 @@ export default defineNavbarConfig([
     items: [
       { text: 'java', link: '/java/', activeMatch: '^/java/' },
       { text: 'dubbo', link: '/dubbo/', activeMatch: '^/dubbo/' },
-      { text: 'golan笔记', link: '/golang/', activeMatch: '^/golang/' },
+      { text: 'golang', link: '/golang/', activeMatch: '^/golang/' },
     ],
   },
 ])
